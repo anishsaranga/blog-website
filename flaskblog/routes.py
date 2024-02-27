8971bfc80893b1de7bb5947f46e9a844a7acbad7
anishsaranga <anishsaranga@gmail.com>
2024-02-27 10:30:57 -0500

    CRUD support for posts added

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -110,7 +110,8 @@ def new_post():
         flash("Posted!", "success")
         return redirect(url_for("home"))
 
-    return render_template("create_post.html", title="New Post", form=form)
+    return render_template("create_post.html", title="New Post", 
+                           form=form, legend="New Post")
 
 
 
@@ -122,7 +123,7 @@ def post(post_id):
 
 
 
-@app.route("/post/<int:post_id>/update")
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -130,4 +131,31 @@ def update_post(post_id):
         abort(403)
     
     form = PostForm()
-    return render_template("create_post.html", title="Update Post", form=form)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        # since already in database, no need to do db.add
+        db.session.commit()
+        flash("Your post has been updated!", "success")
+        return redirect(url_for("post", post_id=post.id))
+    
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template("create_post.html", title="Update Post", 
+                           form=form, legend="Update Post")
+
+
+
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted successfully!", "success")
+    return redirect(url_for("home"))
